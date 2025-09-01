28078d82f77a186764203d30c18b956c80a09cb3
max.lee <f820602h@gmail.com>
2025-09-01 14:31:49 +0800

    region decoration

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,125 @@
-import { defineExtension, useCommand, useIsDarkTheme, watchEffect } from 'reactive-vscode'
-import { window } from 'vscode'
-import { message } from './configs'
-import { logger } from './utils'
+import {
+  type ProviderResult,
+  FoldingRange,
+  FoldingRangeKind,
+  languages,
+  window,
+  Range,
+  OverviewRulerLane,
+} from "vscode";
+import {
+  defineConfigs,
+  computed,
+  defineExtension,
+  useActiveTextEditor,
+  useTextEditorSelection,
+  watchEffect,
+} from "reactive-vscode";
 
-export = defineExtension(() => {
-  logger.info('Extension Activated')
+function getTextLine(fullText: string, regex: RegExp): number[] {
+  const result: number[] = [];
+  let match;
+  while ((match = regex.exec(fullText))) {
+    const startIndex = match.index!;
+    const linesBefore = fullText.substring(0, startIndex).split("\n");
+    result.push(linesBefore.length - 1);
+  }
 
-  useCommand('vue-marker.helloWorld', () => {
-    window.showInformationMessage(message.value)
-  })
+  return result;
+}
 
-  const isDark = useIsDarkTheme()
+export = defineExtension((context) => {
+  const { firstLineColor, blockColor } = defineConfigs("region-plus", {
+    firstLineColor: "string",
+    blockColor: "string",
+  });
+
+  const regionStartLineDecoration = window.createTextEditorDecorationType({
+    isWholeLine: true,
+    backgroundColor: firstLineColor.value,
+    overviewRulerColor: firstLineColor.value,
+    overviewRulerLane: OverviewRulerLane.Full,
+  });
+
+  const regionBlockDecoration = window.createTextEditorDecorationType({
+    isWholeLine: true,
+    backgroundColor: blockColor.value,
+    overviewRulerColor: blockColor.value,
+    overviewRulerLane: OverviewRulerLane.Full,
+  });
+
+  const activeTextEditor = useActiveTextEditor();
+
+  const regionStartLines = computed<number[]>(() => {
+    if (!activeTextEditor.value) return [];
+    return getTextLine(activeTextEditor.value.document.getText(), new RegExp(/\/\/\s*#region/, "g"));
+  });
+  const regionEndLines = computed<number[]>(() => {
+    if (!activeTextEditor.value) return [];
+    return getTextLine(activeTextEditor.value.document.getText(), new RegExp(/\/\/\s*#endregion/, "g"));
+  });
+
+  const regionRangeSet = computed(() => {
+    if (!regionStartLines.value.length || !regionEndLines.value.length) return [];
+    const cloneStartLines = [...regionStartLines.value];
+    return regionEndLines.value.reduce<number[][]>((acc, end) => {
+      const start = cloneStartLines[[...cloneStartLines, end].sort((a, b) => a - b).indexOf(end) - 1];
+      cloneStartLines.splice(start, 1);
+      acc.push([start, end]);
+      return acc;
+    }, []);
+  });
+
+  const regionDecorationRanges = computed(() => {
+    if (!activeTextEditor.value) return [];
+    const editor = activeTextEditor.value;
+    return regionRangeSet.value.map(([start, end]) => {
+      const startLine = editor.document?.lineAt?.(start);
+      const endLine = editor.document?.lineAt?.(end);
+      return {
+        line: new Range(startLine.range.start, startLine.range.end),
+        block: new Range(startLine.range.start, endLine.range.end),
+      };
+    });
+  });
+
+  function updateRegionStartLineDecorations() {
+    if (!activeTextEditor.value) return;
+    const editor = activeTextEditor.value;
+    editor.setDecorations(
+      regionStartLineDecoration,
+      regionDecorationRanges.value.map((range) => range.line)
+    );
+  }
+
+  function updateRegionBlockDecorations() {
+    if (!activeTextEditor.value) return;
+    const editor = activeTextEditor.value;
+    const editorSelection = useTextEditorSelection(editor);
+    editor.setDecorations(
+      regionBlockDecoration,
+      regionDecorationRanges.value.map((range) => range.block).filter((range) => range.contains(editorSelection.value))
+    );
+  }
+
+  context.subscriptions.push(
+    languages.registerFoldingRangeProvider(
+      [
+        { language: "javascript", scheme: "file" },
+        { language: "vue", scheme: "file" },
+      ],
+      {
+        provideFoldingRanges(): ProviderResult<FoldingRange[]> {
+          return regionRangeSet.value.map(([start, end]) => new FoldingRange(start, end, FoldingRangeKind.Region));
+        },
+      }
+    )
+  );
+
+
+
   watchEffect(() => {
-    logger.info('Is Dark Theme:', isDark.value)
-  })
-})
+    updateRegionStartLineDecorations();
+    updateRegionBlockDecorations();
+  });
+});
